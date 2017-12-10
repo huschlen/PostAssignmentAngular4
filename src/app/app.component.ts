93333fe38523140555c07cdfc4b42a9c7d02d70b
huschlen <huschlen@comcast.net>
2017-12-09 20:30:02 -0600

    Add app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+   selector: 'app-root',
+   template: `
+			<app-assignment></app-assignment>
+             `
+})
+export class AppComponent { 
+}
+    
