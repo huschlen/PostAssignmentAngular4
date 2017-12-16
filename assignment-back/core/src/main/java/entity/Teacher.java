b4372e425a06c3bddc5d4b1a6f8f9b266dfb7119
huschlen <huschlen@comcast.net>
2017-12-16 14:14:52 -0600

    Add entity classes

---
+++ assignment-back/core/src/main/java/entity/Teacher.java
@@ -0,0 +1,36 @@
+package com.core.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicUpdate;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "assignment")
+@DynamicUpdate
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Assignment implements Serializable {
+
+    private static final long serialVersionUID  = 1L;
+
+    @Id
+    @Column(name = "id")
+    private int id;
+    
+    @Column(name = "title")
+    private String title;
+    
+    @Column(name = "subject")
+    private String subject;
+
+}
